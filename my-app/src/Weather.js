167e2de9a9d7cd6020def40e07c5ac079e9fa68c
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-15 18:46:05 +0100

    created the Weather.js file and added axios

---
+++ my-app/src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Hello also from Weather</h1>
+    </div>
+  );
+}
